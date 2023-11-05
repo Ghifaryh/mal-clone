621a313fc83376027db5035031451df20a82c928
Ghifary <mhghifaryy@gmail.com>
2023-11-05 22:26:01 +0700

    sesi 1-5

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,18 +1,17 @@
 // import AnimeList from './components/AnimeList';
+import { getAnimeResponse } from '@/app/libs/api-libs';
 import AnimeList from '@/components/AnimeList';
 import Header from '@/components/AnimeList/Header';
 
 const Page = async ({ params }) => {
   const { keyword } = params;
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`
-  );
-  const searchAnime = await response.json();
+  const decodedKeyword = decodeURI(keyword);
+  const searchAnime = await getAnimeResponse('anime', `q=${decodedKeyword}`);
 
   return (
     <>
       <section>
-        <Header title={`Pencarian untuk ${keyword}...`} />
+        <Header title={`Pencarian untuk ${decodedKeyword}...`} />
         <AnimeList api={searchAnime} />
       </section>
     </>

28ea34b820a899a02bd2b239afe1e8f27c00c922
Ghifary <mhghifaryy@gmail.com>
2023-12-17 21:08:20 +0700

    updated to next 14

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,23 +1,25 @@
-'use client';
+"use client"
 
-import { FileSearch } from '@phosphor-icons/react';
-import Link from 'next/link';
+import { FileSearch } from "@phosphor-icons/react"
+import { useRouter } from "next/navigation"
 
 const Page = () => {
+  const router = useRouter()
+
   return (
-    <div className='min-h-screen max-w-xl mx-auto flex justify-center items-center'>
-      <div className='flex justify-center items-center flex-col'>
-        <FileSearch size={44} className='text-color-accent gap-3' />
-        <h3 className='text-color-accent text-4xl font-bold'>Not Found</h3>
-        <Link
-          href='/'
-          className='text-color-primary hover:text-color-accent transition-all'
+    <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
+      <div className="flex justify-center items-center flex-col">
+        <FileSearch size={44} className="text-color-accent gap-3" />
+        <h3 className="text-color-accent text-4xl font-bold">Not Found</h3>
+        <button
+          onClick={() => router.back()}
+          className="text-color-primary hover:text-color-accent transition-all"
         >
           Kembali
-        </Link>
+        </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,15 +1,31 @@
-import { authUserSession } from '@/libs/auth-libs';
-import Image from 'next/image';
+import { authUserSession } from "@/libs/auth-libs"
+import Image from "next/image"
+import Link from "next/link"
 
 const Page = async () => {
-  const user = await authUserSession();
-  return (
-    <div>
-      <h3 className='text-color-primary'>Dashboard</h3>
-      <h3 className='text-color-primary'>Welcome, {user.name} </h3>
-      <Image src={user.image} alt='...' width={250} height={250} />
-    </div>
-  );
-};
+  const user = await authUserSession()
 
-export default Page;
+  return (
+    <div className="mt-8 text-color-primary flex flex-col justify-center items-center">
+      <h5 className="text-2xl font-bold">Welcome, {user?.name} </h5>
+      <Image src={user?.image} alt="..." width={250} height={250} />
+
+      <div className="flex flex-wrap gap-4 py-8">
+        <Link
+          href="/users/dashboard/collection"
+          className="rounded-md bg-color-accent text-color-dark font-bold px-4 py-2 text-xl"
+        >
+          My Collection
+        </Link>
+        <Link
+          href="/users/dashboard/commment"
+          className="rounded-md bg-color-accent text-color-dark font-bold px-4 py-2 text-xl"
+        >
+          My Comment
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Page
